9d0f7e6d76ef56a9df58145cc40f136b0936840a
jhhan <jong.han@cl.cam.ac.uk>
2017-07-06 16:23:16 +0100

    Updated with the timestamp insertion.

--- projects/osnt/sw/host/app/tools/gen_pcap_pkts.py
+++ projects/osnt/sw/host/app/tools/gen_pcap_pkts.py
@@ -38,6 +38,7 @@ script_dir = os.path.dirname(sys.argv[0])
 #Input arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--packet_length", help="Length of packet in no of bytes")
+parser.add_argument("--packet_no", help="No of packets")
 parser.add_argument("--packet_type", type=str, help="TCP, UDP")
 parser.add_argument("--file_name", type=str, help="File name for output pcap")
 parser.add_argument("--src_mac", type=str, help="Source MAC address")
@@ -46,18 +47,28 @@ parser.add_argument("--src_ip", type=str, help="Source IP address")
 parser.add_argument("--dst_ip", type=str, help="Destination IP address")
 parser.add_argument("--sport_no", help="Source Port Number")
 parser.add_argument("--dport_no", help="Destination Port Number")
+parser.add_argument("--packet_ts", type=float, help="Packet timestamp")
 
 args = parser.parse_args()
 
 #Length of Packets
 if (args.packet_length):
    pkt_length = args.packet_length
-   if (int(pkt_length) < 64):  
-      print "\n\nPacket length must be larger than 64!\n"
-      sys.exit()
 else:
    # Default length of packets
    pkt_length = '64'
+
+if (args.packet_ts):
+   pkt_timestamp = args.packet_ts
+else:
+   pkt_timestamp = 0
+
+#No of packets
+if (args.packet_no):
+   no_pkt = args.packet_no
+else:
+	#Default number of packets
+   no_pkt = 1
 
 if (args.file_name):
    pcap_name = args.file_name
@@ -65,9 +76,9 @@ else:
    print "\n\nInput file name for output pcap file\n"
    sys.exit()
 
-if (args.packet_type == 'udp' or args.packet_type == 'UPD'):
+if (args.packet_type == 'udp' or args.packet_type == 'UDP'):
    pkt_type = 'udp'
-   pkt_length = int(pkt_length) - 28
+   pkt_length = int(pkt_length) - 28 - 14
 elif (args.packet_type == 'tcp' or args.packet_type == 'TCP'):
    pkt_type = 'tcp'
    pkt_length = int(pkt_length) - 40 
@@ -117,17 +128,20 @@ for i in range(int(pkt_length)):
 
 pkts_tcp=[]
 #A simple TCP/IP packet embedded in an Ethernet II frame
-pkt = (Ether(src=src_mac_addr, dst=dst_mac_addr)/
-       IP(src=src_ip_addr, dst=dst_ip_addr)/
-       TCP(sport=ip_sport_no, dport=ip_dport_no)/payload_data)
-pkts_tcp.append(pkt)
+for i in range(int(no_pkt)):
+   pkt = (Ether(src=src_mac_addr, dst=dst_mac_addr)/
+          IP(src=src_ip_addr, dst=dst_ip_addr)/
+          TCP(sport=ip_sport_no, dport=ip_dport_no)/payload_data)
+   pkts_tcp.append(pkt)
 
 pkts_udp=[]
 #A simple UDP/IP packet embedded in an Ethernet frame
-pkt = (Ether(src=src_mac_addr, dst=dst_mac_addr)/
-       IP(src=src_ip_addr, dst=dst_ip_addr)/
-       UDP(sport=ip_sport_no, dport=ip_dport_no)/payload_data)
-pkts_udp.append(pkt)
+for i in range(int(no_pkt)):
+   pkt = (Ether(src=src_mac_addr, dst=dst_mac_addr)/
+          IP(src=src_ip_addr, dst=dst_ip_addr)/
+          UDP(sport=ip_sport_no, dport=ip_dport_no)/payload_data)
+   pkt.time = pkt_timestamp
+   pkts_udp.append(pkt)
 
 if (args.packet_type == 'tcp' or args.packet_type == 'TCP'):
    pkts=pkts_tcp
